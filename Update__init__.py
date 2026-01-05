bbd0d360dd78fd108b79b5aaeddb7c42154c3e6c
salouaf <s.florand@cfacampusmontsouris.fr>
2026-01-05 16:23:48 +0100

    Update__init__.py

--- Update__init__.py
+++ Update__init__.py
@@ -32,7 +32,7 @@ def mongraphique():
     return render_template("graphique.html")
 
 @app.route("/histogramme/")
-def histogramme()
+def monhistogramme():
     return render_template("histogramme.html")
   
 if __name__ == "__main__":
